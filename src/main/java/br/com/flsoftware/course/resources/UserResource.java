4684600e49de33f31b854be1e461a9bda5319ac8
Fernando Lopes <nandolopis@gmail.com>
2019-11-04 21:40:52 -0300

    User entity and resource

---
+++ src/main/java/br/com/flsoftware/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package br.com.flsoftware.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.flsoftware.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u =new User(1, "Maria", "Maria@gmail.com", "12321231",	 "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
